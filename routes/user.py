b04841757364ce23dc975aa94aed1cafae9f81e2
richie20502 <richie.lugo.recillas.1990@gmail.com>
2026-04-25 12:16:21 -0600

    routes config model sapp

---
+++ routes/user.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request, jsonify, current_app
+from models.user import User
+
+user_bp = Blueprint('user', __name__, url_prefix='/users')
+
+
+@user_bp.route('/', methods=['POST'])
+def create_user():
+    data = request.get_json()
+
+    user = User(
+        name=data['name'],
+        email=data['email'],
+        password=data['password']  # luego encriptamos
+    )
+    db = current_app.extensions['sqlalchemy'].db
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({"message": "Usuario creado"}), 201
