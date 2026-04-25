7c8da6a1269c9f0af814952e9032db7400544c47
richie20502 <richie.lugo.recillas.1990@gmail.com>
2026-04-25 11:47:20 -0600

    modificacion config y archivo app.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os 
 class  Config:
-    SQLALCHEMY_DATABASE = os.getenv(
+    SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
         "mysql+pymysql://root:@localhost/users_db"
         );
